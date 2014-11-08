e08fbb17bce8e1d213639a4b8656ce7f1da93e05
Juan Alvarez <jlalvarez18@gmail.com>
2014-11-08 14:54:09 -0500

    close #4 - captured images are now uploaded to Dropbox

--- MobiPic/MainViewController.m
+++ MobiPic/MainViewController.m
@@ -8,6 +8,8 @@
 
 #import "MainViewController.h"
 
+#import <Dropbox/Dropbox.h>
+
 @interface MainViewController () <UICollectionViewDelegateFlowLayout, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (nonatomic, strong) UIImagePickerController *pickerController;
@@ -20,6 +22,8 @@
     [super viewDidLoad];
 
     self.title = NSLocalizedString(@"My Photos", nil);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -59,11 +63,26 @@
 }
 
 #pragma mark -
+#pragma mark UICollectionViewDatasource Methods
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CellID" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark -
 #pragma mark UICollectionViewDelegateFlowLayout Methods
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    return CGSizeMake(CGRectGetWidth(self.view.bounds), 320);
+    return CGSizeMake(80, 80);
 }
 
 #pragma mark -
@@ -71,7 +90,27 @@
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-//    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.7);
+    
+    NSString *imageId = [NSUUID UUID].UUIDString;
+    NSString *imageName = [imageId stringByAppendingPathExtension:@"jpg"];
+    
+    NSError *error;
+    DBPath *imagePath = [[DBPath root] childPath:imageName];
+    DBFile *imageFile = [[DBFilesystem sharedFilesystem] createFile:imagePath error:&error];
+    
+    if (error) {
+        NSLog(@"%@", error);
+    } else {
+        if ([imageFile writeData:imageData error:&error]) {
+            [picker dismissViewControllerAnimated:YES completion:^{
+                [self.collectionView reloadData];
+            }];
+        } else {
+            NSLog(@"%@", error);
+        }
+    }
     
     NSLog(@"%@", info);
 }
