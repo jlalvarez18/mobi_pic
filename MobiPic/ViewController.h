d08276dfecfeb248f0ca73fab5ce9bba6e4ed531
Juan Alvarez <jlalvarez18@gmail.com>
2014-11-08 12:53:59 -0500

    created Xcode project

---
+++ MobiPic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobiPic
+//
+//  Created by Juan Alvarez on 11/8/14.
+//  Copyright (c) 2014 Alvarez Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
